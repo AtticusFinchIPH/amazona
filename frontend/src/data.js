66c7dadccdb9ec84b2f17250126e0baf2296460a
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-06-23 12:05:35 +0200

    Build ProductScreen

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -5,7 +5,7 @@ export default {
             name: "Slim shirt",
             image: "/images/d1.jpg",
             brand: "Nike",
-            price: "$60",
+            price: "60",
             rating: 4.5,
             review: 10,
             category: "shirts"
@@ -15,7 +15,7 @@ export default {
             name: "Nice shirt",
             image: "/images/d1.jpg",
             brand: "Nike",
-            price: "$80",
+            price: "80",
             rating: 4.5,
             review: 25,
             category: "shirts"
@@ -25,7 +25,7 @@ export default {
             name: "Long pants",
             image: "/images/d1.jpg",
             brand: "Nike",
-            price: "$90",
+            price: "90",
             rating: 4.5,
             review: 6,
             category: "pants"
