7233a10055bb359c4b3066bf8978a50fd29e9b57
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-06-23 18:22:34 +0200

    Setup backend and get products using axios

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [
+        {
+            _id: "1",
+            name: "Slim shirt",
+            image: "/images/d1.jpg",
+            brand: "Nike",
+            price: "60",
+            rating: 4.5,
+            review: 10,
+            category: "shirts"
+        },
+        {
+            _id: "2",
+            name: "Nice shirt",
+            image: "/images/d1.jpg",
+            brand: "Nike",
+            price: "80",
+            rating: 4.5,
+            review: 25,
+            category: "shirts"
+        },
+        {
+            _id: "3",
+            name: "Long pants",
+            image: "/images/d1.jpg",
+            brand: "Nike",
+            price: "90",
+            rating: 4.5,
+            review: 6,
+            category: "pants"
+        }
+    ]
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
-import data from './data';
 import HomeScreen from './Screen/HomeScreen';
 import ProductScreen from './Screen/ProductScreen';
 
